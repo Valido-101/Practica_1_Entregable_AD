f05423ca87bb5ae618fb8313dbe12691fa10fc87
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-25 21:39:43 +0100

    Parte 3 con comentarios

--- src/Parte_3/GestorFruteria.java
+++ src/Parte_3/GestorFruteria.java
@@ -3,7 +3,6 @@ package Parte_3;
 import java.io.File;
 import java.io.IOException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
@@ -36,6 +35,7 @@ public class GestorFruteria {
 		
 	}
 	
+	//Ejemplo de clase para crear un elemento
 	public void crearElemento(String nombre,long precio) throws SAXException, IOException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException 
 	{
 
@@ -61,30 +61,58 @@ public class GestorFruteria {
 	public void modificarElemento(String articulo, float precio ) 
 	{
 	        try {
-	            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	              DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	              Document doc = dBuilder.parse(archivo_xml);
-
-	              NodeList nod = doc.getElementsByTagName("articulo");
-
-	              for(int i = 0; i < nod.getLength(); i++) {
-
-	                  Node nNode = nod.item(i);
-
-	                  if(nNode.getNodeType() == Node.ELEMENT_NODE) {
-	                    Element eElement = (Element) nNode;
-
-	                    if( eElement.getElementsByTagName("nombre").item(0).getTextContent().equalsIgnoreCase(articulo)) {
-	                        eElement.getElementsByTagName("precio").item(0).setTextContent(String.valueOf(precio));
+	        	
+	        	  //Instanciamos un nuevo objeto Document
+	        	  Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(archivo_xml);
+	
+	        	  //Lista de nodos articulo
+	              NodeList articulos = doc.getElementsByTagName("articulo");
+	              
+	              //Instanciamos la variable existe
+	              boolean existe = false;
+	
+	              //Bucle que recorre la longitud del nodelist
+	              for(int i = 0; i < articulos.getLength(); i++) {
+	
+	            	  //Se creará un objeto de tipo nodo por cada elemento del nodelist
+	                  Node nodo_articulo = articulos.item(i);
+	
+	                  //Se comprueba si el nodo es de tipo elemento (es decir, no es un atributo, por ejemplo)
+	                  if(nodo_articulo.getNodeType() == Node.ELEMENT_NODE) {
+	                	  
+	                	//Si es así, el nodo se castea a elemento para poder hacer las siguientes comprobaciones
+	                    Element elemento = (Element) nodo_articulo;
+	
+	                    //Si el texto del primer elemento con la etiqueta nombre coincide con el nombre de artículo introducido
+	                    //se cambia el contenido del elemento precio por el nuevo precio que se ha introducido
+	                    if(elemento.getElementsByTagName("nombre").item(0).getTextContent().equalsIgnoreCase(articulo)) 
+	                    {
+	                    	elemento.getElementsByTagName("precio").item(0).setTextContent(String.valueOf(precio));
+	                    	//Se informa al usuario
 	                        System.out.println("Precio cambiado correctamente");
+	                        //La variable existe pasa a ser true
+	                        existe = true;
+	                        //Se crea un objeto TransformerFactory
+	                        Transformer transf = TransformerFactory.newInstance().newTransformer();
+	                        //Se crea un objeto DOMSource para determinar cuál es el documento modificado que vamos a guardar
+	      	                DOMSource origen = new DOMSource(doc);
+	      	                //Se crea un objeto StreamResult para determinar cuál es el archivo en el que vamos a guardar el
+	      	                //xml modificado
+	      	                StreamResult destino = new StreamResult(archivo_xml);
+	      	                //Se llama al método correspondiente para que se guarde el xml modificado
+	      	                transf.transform(origen,destino);
 	                    }
-
+	
 	                  }
 	                }
-	              Transformer transf = TransformerFactory.newInstance().newTransformer();
-	                DOMSource origen = new DOMSource(doc);
-	                StreamResult destino = new StreamResult(archivo_xml);
-	                transf.transform(origen,destino);
+	              
+	              //Si la variable existe es false, se indica al usuario que el artículo que ha introducido no existe
+	              if(existe==false) 
+	              {
+	            	  
+	            	  System.out.println("\nLo sentimos, el artículo introducido no existe.\n");
+	            	  
+	              }
 
 	        }catch(Exception e) {
 	              e.printStackTrace();
@@ -94,70 +122,108 @@ public class GestorFruteria {
 	public void fueraDeStock (String articulo) throws SAXException, IOException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException 
 	{
 		
+		   //Instanciamos las variables que necesitamos para almacenar el valor del elemento que vamos a mover
 		   String nombre_articulo = "", precio = "";
 
-	       Document doc = (Document) DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(archivo_xml);
-	       NodeList items = doc.getElementsByTagName("articulo");
-	        for (int i = 0; i < items.getLength(); i++) {
-	            Element element = (Element) items.item(i);
-	            // elegir un elemento especifico por algun atributo
-	            if (element.getElementsByTagName("nombre").item(0).getTextContent().equalsIgnoreCase(articulo)) {
-	                // borrar elemento
-	            	nombre_articulo=element.getElementsByTagName("nombre").item(0).getTextContent();
-	            	precio=element.getElementsByTagName("precio").item(0).getTextContent();
-	                element.getParentNode().removeChild(element);
+		   //Documento fuente que usaremos para modificar
+	       Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(archivo_xml);
+	       
+	       //Instanciamos un nodelist que guarde todos los elementos con la etiqueta articulo
+	       NodeList articulos = doc.getElementsByTagName("articulo");
+	       
+	       //Variable para saber si el artículo en cuestión existe
+	       boolean existe=false;
+	       
+	        //Bucle que recorre el nodelist
+	        for (int i = 0; i < articulos.getLength(); i++) 
+	        {
+	        	//Se castea el elemento actual del nodelist a un objeto Element
+	            Element elemento = (Element) articulos.item(i);
+	            //SSe comprueba si el elemento actual tiene otro elemento cuyo texto coincide con el introducido por parámetro
+	            if (elemento.getElementsByTagName("nombre").item(0).getTextContent().equalsIgnoreCase(articulo)) {
+	                //Si coincide, se guardan los valores nombre y precio y se borra el elemento de en-stock
+	            	nombre_articulo=elemento.getElementsByTagName("nombre").item(0).getTextContent();
+	            	precio=elemento.getElementsByTagName("precio").item(0).getTextContent();
+	            	elemento.getParentNode().removeChild(elemento);
+	                //Existe pasa a ser true
+	                existe=true;
+	                //Creamos un elemento padre articulo
+	                Element elementoPadre = doc.createElement("articulo");
+	                //Creamos un elemento hijo nombre y lo asignamos a articulo
+	    			elementoPadre.appendChild(doc.createElement("nombre"));
+	    			//Creamos un elemento hijo precio y lo asignamos a articulo
+	    			elementoPadre.appendChild(doc.createElement("precio"));
+	    			//Asignamos a nombre y precio loss valores que tenían anteriormente
+	    			elementoPadre.getChildNodes().item(0).appendChild(doc.createTextNode(nombre_articulo));
+	    			elementoPadre.getChildNodes().item(1).appendChild(doc.createTextNode(String.valueOf(precio)));
+	    			//Asignamos al elemento fuera-stock el elemento artículo que hemos creado con suss elementos hijos
+	    			doc.getElementsByTagName("fuera-stock").item(0).appendChild(elementoPadre);
+	    			//Creamos un objeto del tipo TransformerFactory
+	    			Transformer transf = TransformerFactory.newInstance().newTransformer();
+                    //Se crea un objeto DOMSource para determinar cuál es el documento modificado que vamos a guardar
+	    	        DOMSource origen = new DOMSource(doc);
+	    	        //Se crea un objeto StreamResult para determinar cuál es el archivo en el que vamos a guardar el
+  	                //xml modificado
+	    	        StreamResult destino = new StreamResult(archivo_xml);
+  	                //Se llama al método correspondiente para que se guarde el xml modificado
+	    	        transf.transform(origen, destino);
 	            }
 	        }
 	        
-	        Element elementoPadre = doc.createElement("articulo");
-			elementoPadre.appendChild(doc.createElement("nombre"));
-			elementoPadre.appendChild(doc.createElement("precio"));
-			elementoPadre.getChildNodes().item(0).appendChild(doc.createTextNode(nombre_articulo));
-			//convertir el precio a string
-			elementoPadre.getChildNodes().item(1).appendChild(doc.createTextNode(String.valueOf(precio)));
-			doc.getElementsByTagName("fuera-stock").item(0).appendChild(elementoPadre);
-	        
-	        Transformer transf = TransformerFactory.newInstance().newTransformer();
-	        
-	        
-
-	        // 3. Exportar nuevamente el XML
-	        DOMSource origen = new DOMSource(doc);
-	        StreamResult destino = new StreamResult(archivo_xml);
-	        transf.transform(origen, destino);
+            //Si la variable existe es false, se indica al usuario que el artículo que ha introducido no existe
+	        if(existe==false) 
+	        {
+	        	
+	        	System.out.println("\nLo sentimos, el artículo introducido no existe.\n");
+	        	
+	        }      
 	    }
 	
 	
 	public void mostrarArticulos() throws SAXException, IOException, ParserConfigurationException 
 	{
-		
-		Document doc = (Document) DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(archivo_xml);
-	    NodeList items = doc.getElementsByTagName("articulo");
-	    NodeList fuera_stock = doc.getElementsByTagName("fuera-stock");
+		//Documento fuente que usaremos para modificar
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(archivo_xml);
+	    //Instanciamos un nodelist que guarde todos los elementos con la etiqueta articulo
+		NodeList nodos = doc.getElementsByTagName("articulo");
+	    //Instanciamos un nodo que guarde el elemnto con la etiqueta fuera-stock
+	    Node fuera_stock = doc.getElementsByTagName("fuera-stock").item(0);
+	    //Contador que necesitaremos para que el formato de salida sea el especificado
 	    cont_fuera_stock=0;
+	    //Id de artículo
 	    id_articulo=1;
 	    
+	    //Inicio del apartado de artículos en stock
 	    System.out.println("/--ARTÍCULOS EN STOCK--/\n");
 	    
-	    for(int temp = 0; temp < items.getLength(); temp++) 
+	    //Bucle que recorre todos los nodos del nodelist
+	    for(int x = 0; x < nodos.getLength(); x++) 
 	    {
 	    	
-	    	Node nNode = items.item(temp);
+	    	//Nodo actual del nodelist
+	    	Node nodo_articulo = nodos.item(x);
 	    	
-	    	if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+	    	//Comprobamos si el nodo actual es del tipo elemento
+	    	if(nodo_articulo.getNodeType() == Node.ELEMENT_NODE) 
+	    	{
 	    	
-	    	  
-	    	    Element eElement = (Element) nNode;
+	    	    //Si lo es, hacemos un casteo para poder usar los métodos de la clase Element
+	    	    Element elemento = (Element) nodo_articulo;
 	    	    
-	    	    if(eElement.getParentNode()==fuera_stock.item(0) && cont_fuera_stock==0) 
+	    	    //Si el elemento padre del elemento actual es fuera-stock y el contador es igual a 0 (es el primer elemento de este elemento padre), se imprime esto por pantalla 
+	    	    if(elemento.getParentNode()==fuera_stock && cont_fuera_stock==0) 
 	    	    {
 	    	    	System.out.println("\n/--ARTÍCULOS FUERA DE STOCK--/\n");
+	    	    	//Se aumenta el contador para que no se vuelva a escribir
 	    	    	cont_fuera_stock++;
+	    	    	//Se resetea el id de artículo a 1
 	    	    	id_articulo=1;
 	    	    }
 	
-	    	    System.out.println("\nArtículo: "+id_articulo+", Nombre: " + eElement.getElementsByTagName("nombre").item(0).getTextContent()+", Precio: "+ eElement.getElementsByTagName("precio").item(0).getTextContent());
+	    	    //Se muestran los datos de este elemento
+	    	    System.out.println("\nArtículo: "+id_articulo+", Nombre: " + elemento.getElementsByTagName("nombre").item(0).getTextContent()+", Precio: "+ elemento.getElementsByTagName("precio").item(0).getTextContent());
 	    	
+	    	    //Se aumenta el id
 	    	    id_articulo++;
 	    	    
 	    	}
--- src/Parte_3/Principal.java
+++ src/Parte_3/Principal.java
@@ -14,24 +14,32 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		//Escáner necesario para introducir datos
 		Scanner teclado = new Scanner(System.in);
 		
+		//Variable que guarda la elección del usuario
 		int opcion;
 		
+		//Nombre de artículo que introducirá el usuario
 		String nombre_articulo;
 		
+		//Precio de artículo que introducirá el usuario
 		float precio;
 		
+		//Instancia de la clase con todos los métodos
 		GestorFruteria gestor = new GestorFruteria(new File("res/registro.xml"));
 		
+		//Bucle infinito
 		while(true) 
 		{
 			System.out.println("Introduzca la opción deseada: \n1: Modificar artículo\n 2: Mover un elemento a fuera de stock\n 3: Mostrar artículos\n 4: Salir\n >");
 			opcion=teclado.nextInt();
 			
+			//Switch case con el menú
 			switch(opcion) 
 			{
 				case 1:
+						//Limpieza del búfer de teclado
 						teclado.nextLine();
 						System.out.println("Introduzca el nombre del artículo a modificar: ");
 						nombre_articulo=teclado.nextLine();
@@ -41,6 +49,7 @@ public class Principal {
 					break;
 				
 				case 2:
+					//Limpieza del búfer de teclado
 					teclado.nextLine();
 					System.out.println("Introduzca el nombre del artículo a mover: ");
 					nombre_articulo=teclado.nextLine();
@@ -64,7 +73,9 @@ public class Principal {
 				
 				default:
 					System.out.println("¡Hasta pronto!");
+					//Se cierra el teclado
 					teclado.close();
+					//Se cierra el programa
 					System.exit(0);
 				break;
 			}
