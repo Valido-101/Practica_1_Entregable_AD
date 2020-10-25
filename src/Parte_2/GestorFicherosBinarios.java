e62a8d928662b1c9c54a73174629f33d11596a4f
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-25 20:46:50 +0100

    Comentarios añadidos a la Parte 2 de la práctica

--- src/Parte_2/GestorFicherosBinarios.java
+++ src/Parte_2/GestorFicherosBinarios.java
@@ -8,10 +8,14 @@ import java.io.RandomAccessFile;
 import java.util.InputMismatchException;
 
 public class GestorFicherosBinarios {
+
+	//Atributos de la clase
 	
 	private File f1;
+
 	private RandomAccessFile flujo;
 	
+	//Constructor de la clase GestorFicherosBinarios que recibe un objeto File por parámetro
 	public GestorFicherosBinarios(File f1) 
 	{
 		
@@ -23,6 +27,7 @@ public class GestorFicherosBinarios {
 	{
 		
 		try {
+			//Se crea un nuevo objeto RandomAccessFile con permisos de lectura y escritura
 			flujo=new RandomAccessFile(f1,"rw");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -30,25 +35,31 @@ public class GestorFicherosBinarios {
 		}
 		
 		try {
+			//Nos aseguramos de estar situados al inicio del documento
 			flujo.seek(0);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
+		//Bucle que recorre el array de apellidos e introduce los valores de cada array (puesto que todos tienen la misma longitud)
 		for(int x=0 ; x<apellidos.length ; x++) 
 		{
 			
 			try {
+				//Escribimos primero el id del empleado
 				flujo.writeInt(x+1);
+				//Recorremos la posición x del array de apellidos y la llenamos de espacios para que tenga justo 10 caracteres
 				while(apellidos[x].length()<10) 
 				{
 					apellidos[x]+=" ";
 				}
+				//Escribimos la posición x del array de apellidos en bytes
 				flujo.writeBytes(apellidos[x].toString());
+				//Escribimos el int Departamento
 				flujo.writeInt(departamento[x]);
+				//Escribimos el float Salario
 				flujo.writeFloat(salario[x]);
-				System.out.println(flujo.length());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -57,6 +68,7 @@ public class GestorFicherosBinarios {
 		}
 		
 		try {
+			//Cerramos el RandomAccessFile
 			flujo.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -70,21 +82,31 @@ public class GestorFicherosBinarios {
 	{
 		
 		try {
+			//Instanciamos un objeto RandomAccessFile con permiso de lectura únicamente, puesto que sólo queremos mostrar datos
 			flujo = new RandomAccessFile(f1,"r");
 				
+			//Mientras la posición actual del puntero sea menor o igual a la longitud en bytes del fichero, se repite el bucle
 			while(flujo.getFilePointer()<=flujo.length()) 
 			{
 				
 				try 
 				{
+					//Leemos un int y lo imprimimos por pantalla (id de empleado)
 					System.out.print("Id: "+flujo.readInt());
+					//Creamos un array de bytes con una longitud de 10
 					byte[] buff = new byte[10];
+					//Leemos esa cantidad de bytes (apellido)
 					flujo.read(buff);
+					//Almacenamos el valor en String del array de bytes
 					String apellido = new String(buff);
+					//Lo imprimimos por pantalla con el método trim
 					System.out.print(", Apellido: "+apellido.trim());
+					//Leemos e imprimimos el int departamento
 					System.out.print(", Departamento: "+flujo.readInt());
+					//Leemos e imprimimos el float salario
 					System.out.print(", Salario: "+flujo.readFloat()+"\n");			
 				}
+				//Si llegamos al final del archivo antes de que se corte el bucle, recogemos la excepción y salimos del bucle para evitar errores
 				catch(EOFException e) 
 				{
 					break;
@@ -92,6 +114,7 @@ public class GestorFicherosBinarios {
 			
 		}
 		
+		//Cerramos el RandomAccessFile
 		flujo.close();
 		
 			
@@ -110,18 +133,22 @@ public class GestorFicherosBinarios {
 	{
 		
 		try {
+			//Creamos un RandomAccessFile con únicamente permiso de lectura
 			flujo = new RandomAccessFile(f1,"r");
 			
-			//Esto coloca el puntero a la altura del siguiente id
+			//Esto coloca el puntero al final del registro anterior
 			flujo.seek(22*(id-1));
 				
+			//Seguimos exactamente el mismo proceso de lectura que en el método leerDatos
 			System.out.print("Id: "+flujo.readInt());
 			byte[] buff = new byte[10];
 			flujo.read(buff);
 			String apellido = new String(buff);
 			System.out.print(", Apellido: "+apellido);
 			System.out.print(", Departamento: "+flujo.readInt());
-			System.out.print(", Salario: "+flujo.readFloat()+"\n");			
+			System.out.print(", Salario: "+flujo.readFloat()+"\n");		
+			
+			//Cerramos el RandomAccessFile
 			flujo.close();
 			
 		} catch (FileNotFoundException e) {
@@ -136,17 +163,21 @@ public class GestorFicherosBinarios {
 	
 	public void insertarDatos(int id, String apellido, int departamento, float salario)
 	{
+		//Instanciamos una variable que nos servirá para saber si el empleado ya existe
 		boolean existe = false;
 		try {
+			//Instanciamos un nuevo objeto RandomAccessFile
 			flujo = new RandomAccessFile(f1,"rw");
 			
-			//Esto coloca el puntero a la altura del siguiente id
+			//Esto coloca el puntero a la altura del final del registro anterior
 			flujo.seek(22*(id-1));
 				
 				try 
 				{
+					//Leemos el id a la altura del id introducido para comprobar si existe ese empleado
 					if(flujo.readInt()==id) 
 					{
+						//Si el id leído es igual al introducido, se informa al usuario y la variable existe pasa a ser true
 						System.out.println("Este empleado ya existe.");
 						existe=true;
 					}
@@ -155,11 +186,13 @@ public class GestorFicherosBinarios {
 					
 				}
 			
-				
+			//Si la variable existe es false (no existe el empleado), se introducen los datos en el fichero	
 			if(existe==false) 
 			{
 				try {
+					//Colocamos el puntero al final del fichero
 					flujo.seek(flujo.length());
+					//Seguimos exactamente el mismo proceso de escritura que en el método escribirDatos
 					flujo.writeInt(id);
 					while(apellido.length()<10) 
 					{
@@ -169,17 +202,18 @@ public class GestorFicherosBinarios {
 					flujo.writeInt(departamento);
 					flujo.writeFloat((float)salario);
 					
+					//Se informa al usuario de que la tarea se ha realizado con éxito
 					System.out.println("Tarea realizada con éxito");
 				}
+				//Si se introduce algún dato erróneo se informa al usuario
 				catch(InputMismatchException e) 
 				{
 					System.out.println("Dato no válido.");
 					System.out.println(e.getCause());
 				}
 			}
-				
-				
-			
+					
+			//Cerramos el RandomAccessFile
 			flujo.close();
 			
 		} catch (FileNotFoundException e) {
--- src/Parte_2/Principal.java
+++ src/Parte_2/Principal.java
@@ -7,50 +7,68 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		//Instanciamos un objeto de la clase GestorFicherosBinarios, donde tenemos todos los métodos
 		GestorFicherosBinarios gestor = new GestorFicherosBinarios(new File("src/Parte_2/Empleados.dat"));
+		
+		//Instanciamos un escáner
 		Scanner teclado = new Scanner(System.in);
-		Scanner teclado2 = new Scanner(System.in);
+		
+		//Instanciamos una variable para elegir una opción del menú
 		int opcion;
+		
+		//Instanciamos una variable para cada dato de empleado
 		int id_empleado;
 		String apellido;
 		int departamento;
 		float salario;
 		
+		//Arrays de String, int y float que guardan los apellidos, departamentos y salarios que deberemos introducir en el método escribirDatos
 		String[] apellidos = {"ALONSO","RODRIGUEZ","OTERO","JIMENEZ","MEJIAS"};
 		
 		int[] departamentos = {10, 80, 40, 20, 10};
 		
 		float[] salarios =  {(float) 4000.45, (float) 2500.03, (float) 1100.25, (float) 1687.30, (float) 6300.40};
 		
+		//Bucle infinito que sólo se detendrá cuando escojamos la opción 5.
 		while(true) 
 		{
+			
 			System.out.println("Bienvenido al menú. Elija una opción: \n 1: Escribir datos en fichero \n 2: Leer datos \n 3: Consultar datos \n 4: Insertar empleado \n 5: Salir");
 			
+			//Se introduce la opción deseada del menú
 			opcion = teclado.nextInt();
 			
+			//Se hace un switch case para que se realice un método distinto dependiendo de la opción
 			switch(opcion) 
 			{
 			
 				case 1:
+						//Si se introduce 1 se llama al método escribir datos, que recibe por parámetros los arrays del principio
 						gestor.escribirDatos(apellidos, departamentos, salarios);
 						System.out.println("Datos escritos correctamente\n");
 					break;
 					
 				case 2: 
+						//Si se introduce 2 se llama al método que lee el documento y muestra los datos por pantalla
 						gestor.leerDatos();
 					break;
 					
 				case 3:
+						//Si se pulsa 3 se pide por teclado el id del empleado que se quiere buscar
 						System.out.println("Introduzca el id del empleado que quiera buscar:");
 						id_empleado = teclado.nextInt();
+						//Se llama al método consultarDatos y se pasa por parámetro el id del empleado que se quiera buscar
 						gestor.consultarDatos(id_empleado);
 					break;
 					
 				case 4:
+						//Si se introduce 4 se piden los datos del nuevo empleado que se quiera insertar
 						System.out.print("Introduzca los datos del nuevo empleado:\n Id: ");
 						id_empleado=teclado.nextInt();
+						//Hacemos una limpieza del búfer de teclado para que no haya problemas al introducir un String después de un int
+						teclado.nextLine();
 						System.out.print("Apellido: ");
-						apellido=teclado2.nextLine();
+						apellido=teclado.nextLine();
 						System.out.print("Departamento: ");
 						departamento=teclado.nextInt();
 						System.out.print("Salario: ");
@@ -59,13 +77,14 @@ public class Principal {
 						//un punto como separador entre entero y decimal y me daba un InputMismatchException, haciéndome pensar
 						//que algo estaba mal :)
 						salario=teclado.nextFloat();
+						//Se llama al método insertarDatos y se le pasa por parámetro los datos nuevos a insertar
 						gestor.insertarDatos(id_empleado, apellido, departamento, salario);
 					break;
 					
 				case 5: 
+						//Si se pulsa 5 el programa se despide del usuario, cierra el escáner y sale del programa.
 						System.out.println("¡Hasta pronto!");
 						teclado.close();
-						teclado2.close();
 						System.exit(0);
 					break;
 			
